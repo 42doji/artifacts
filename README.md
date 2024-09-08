2a10219c31ce5902569c751a06935fcc2ceb12bd
doji <doji@student.42gyeongsan.kr>
2024-09-08 12:00:38 +0900

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# artifacts
+
+Developed with Unreal Engine 5
---
+++ Source/artifacts/artifacts.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "artifacts.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, artifacts, "artifacts" );
